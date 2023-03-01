f04ac124a802f444bf200c90672048b55d9772e3
JiquanWang99 <951873902@qq.com>
2023-03-02 00:02:27 +0800

    chore: change test file name

--- test/schema2ts.test.ts
+++ test/schema2ts.test.ts
@@ -6,11 +6,11 @@ import schema2ts from '../src';
 describe('Is the output correct', () => {
   test('schema2ts', () => {
     const input = fs.readFileSync(
-      path.join(__dirname, './schema1.json'),
+      path.join(__dirname, './test1/input.json'),
       'utf-8',
     );
     const shouldOutput = fs.readFileSync(
-      path.join(__dirname, './output1.ts'),
+      path.join(__dirname, './test1/output.ts'),
       'utf-8',
     );
 
