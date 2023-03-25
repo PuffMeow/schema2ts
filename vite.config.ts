a9973140063cf5b2ad202358689875417a975239
JiquanWang99 <951873902@qq.com>
2023-03-25 19:01:30 +0800

    chore: change build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import typescript from '@rollup/plugin-typescript';
+import path, { resolve } from 'path';
+import ts from '@rollup/plugin-typescript';
 
 export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'schema2ts',
-      fileName: 'schema2ts',
+      fileName: 'src/index',
     },
   },
-  plugins: [typescript()],
+  plugins: [ts()],
+  test: {},
 });
